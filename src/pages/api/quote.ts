09b8a5d7dac7e7090b524d98972f35ee4dd69e9a
onkelarmin <97581983+onkelarmin@users.noreply.github.com>
2026-02-09 15:20:42 +1000

    built quote functionality

---
+++ src/pages/api/quote.ts
@@ -0,0 +1,31 @@
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async () => {
+  try {
+    const res = await fetch("https://zenquotes.io/api/random");
+
+    if (!res.ok) {
+      console.error("Upstream error:", res.status);
+
+      return new Response(
+        JSON.stringify({ error: `Upstream service failed` }),
+        { status: 502 },
+      );
+    }
+
+    const data = await res.json();
+
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Unexpected error:", error);
+
+    return new Response(JSON.stringify({ error: "Internal server error" }), {
+      status: 500,
+    });
+  }
+};
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,7 +1,9 @@
 import { initAnimations } from "@scripts/features/animations";
 import { initShowMore } from "./features/showMore";
+import { initDisplayQuote } from "./features/quote/displayQuote";
 
 document.addEventListener("DOMContentLoaded", () => {
   initAnimations();
   initShowMore();
+  initDisplayQuote();
 });
