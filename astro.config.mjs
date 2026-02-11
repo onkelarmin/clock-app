a11ae01f8a7e926d34aac66f35468c8b70a48c56
onkelarmin <97581983+onkelarmin@users.noreply.github.com>
2026-02-11 19:47:15 +1000

    added Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   site: "https://fm-recipe-finder-website.netlify.app/",
+
   image: {
     responsiveStyles: true,
     layout: "constrained",
   },
+
   devToolbar: {
     enabled: false,
   },
+
+  adapter: netlify(),
 });
