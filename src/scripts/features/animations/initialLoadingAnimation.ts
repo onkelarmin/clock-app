cc8da589b8d73ed54dba6426c81b70bf63c5d5af
onkelarmin <97581983+onkelarmin@users.noreply.github.com>
2026-02-13 07:40:40 +1000

    fixed animation error

--- src/scripts/features/animations/initialLoadingAnimation.ts
+++ src/scripts/features/animations/initialLoadingAnimation.ts
@@ -24,9 +24,9 @@ export function initInitialLoadingAnimation() {
         },
       })
       .to(main, { autoAlpha: 1, duration: 0.1 })
-      .to(".bg-image > *", {
-        scale: reduceMotion ? 0 : isDesktop ? 1.07 : 0,
-        duration: 3,
+      .to(".bg-image", {
+        scale: reduceMotion ? 1 : isDesktop ? 1.07 : 1,
+        duration: 2.7,
         ease: "power1.out",
       })
       .from(
@@ -35,7 +35,7 @@ export function initInitialLoadingAnimation() {
           y: reduceMotion ? 0 : isDesktop ? 40 : 0,
           opacity: 0,
           filter: "blur(8px)",
-          stagger: 0.1,
+          stagger: 0.2,
           duration: 1,
           ease: "power2.out",
         },
@@ -47,7 +47,7 @@ export function initInitialLoadingAnimation() {
           y: reduceMotion ? 0 : isDesktop ? 40 : 0,
           opacity: 0,
           filter: "blur(8px)",
-          duration: 1.5,
+          duration: 1.2,
           ease: "power2.out",
         },
         "<40%",
@@ -57,10 +57,10 @@ export function initInitialLoadingAnimation() {
         {
           opacity: 0,
           filter: "blur(8px)",
-          duration: 1.5,
+          duration: 1.2,
           ease: "power2.out",
         },
-        "<40%",
+        "<20%",
       );
   });
 }
